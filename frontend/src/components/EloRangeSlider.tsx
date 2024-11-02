38187c9c7f7fff4da523d63b4c9e2f95ac28a7fe
SheeNigami <sheenhern@outlook.com>
2024-11-02 11:16:09 +0800

    working elo filter slider

---
+++ frontend/src/components/EloRangeSlider.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import * as Slider from "@radix-ui/react-slider";
+
+interface EloRangeSliderProps {
+  value: [number, number];
+  onValueChange: (value: [number, number]) => void;
+}
+
+export function EloRangeSlider({ value, onValueChange }: EloRangeSliderProps) {
+  return (
+    <div className="flex flex-col space-y-2">
+      <div className="flex justify-between">
+        <span className="text-sm">ELO Range</span>
+        <span className="text-sm">{value[0]} - {value[1]}</span>
+      </div>
+      <Slider.Root
+        className="relative flex items-center select-none touch-none w-full h-5"
+        value={value}
+        onValueChange={onValueChange}
+        max={3000}
+        step={50}
+        minStepsBetweenThumbs={1}
+      >
+        <Slider.Track className="bg-gray-700 relative grow rounded-full h-2">
+          <Slider.Range className="absolute bg-blue-600 rounded-full h-full" />
+        </Slider.Track>
+        <Slider.Thumb className="block w-5 h-5 bg-white rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-600" />
+        <Slider.Thumb className="block w-5 h-5 bg-white rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-600" />
+      </Slider.Root>
+    </div>
+  );
+}
